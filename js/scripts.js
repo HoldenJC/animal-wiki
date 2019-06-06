5f1643de11cc8a8353d6d8434d0e7f98448e91c3
Emerson Jordan and Holden Clark <emerson@>
2019-06-06 09:41:31 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $("#gs1").click(function(){
+    $("#godsword1Info").toggle();
+  });
+  $("#gs2").click(function(){
+    $("#godsword2Info").toggle();
+  });
+  $("#gs3").click(function(){
+    $("#godsword3Info").toggle();
+  });
+  $("#gs4").click(function(){
+    $("#godsword4Info").toggle();
+  });
+
+});
