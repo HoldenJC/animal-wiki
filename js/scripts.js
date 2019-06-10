ca382b7eee624c8123fd19980df8bd7f978eac9b
Emerson Jordan and Holden Clark <emerson@>
2019-06-10 15:37:11 -0700

    refactor event listener javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
 $(function(){
-  $("#gs1").click(function(){
-    $("#godsword1Info").toggle();
-  });
-  $("#gs2").click(function(){
-    $("#godsword2Info").toggle();
-  });
-  $("#gs3").click(function(){
-    $("#godsword3Info").toggle();
-  });
-  $("#gs4").click(function(){
-    $("#godsword4Info").toggle();
+
+var imgIds = ["#gs1", "#gs2", "#gs3", "#gs4"];
+var divIds = ["#godsword1Info", "#godsword2Info", "#godsword3Info", "#godsword4Info"];
+debugger;
+  imgIds.forEach(function(imgId){
+    $(imgId).click(function(){
+      $(divIds[imgIds.indexOf(imgId)]).toggle();
+    });
+
   });
 });
+
+// $(divIds[imgIds.indexOf(imgId)]).toggle();
+//
+// $("#godsword1Info").toggle();
+
+// $(function(){
+//   $("#gs1").click(function(){
+//     $("#godsword1Info").toggle();
+//   });
+//   $("#gs2").click(function(){
+//     $("#godsword2Info").toggle();
+//   });
+//   $("#gs3").click(function(){
+//     $("#godsword3Info").toggle();
+//   });
+//   $("#gs4").click(function(){
+//     $("#godsword4Info").toggle();
+//   });
+// });
 
 //learning through failure below
 
@@ -29,9 +46,6 @@ $(function(){
 //   }
 //
 // });
-
-
-
 
 // $(function(){
 //
